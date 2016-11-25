237c6b98fcb20e97b4b1a64a696eb8cd38ff14e8
priom <priom.galaxy@gmail.com>
2016-11-25 05:18:10 -0400

    main component created

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,14 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Router, IndexRoute, hashHistory } from 'react-router';
+import Main from 'Main';
 
 ReactDOM.render(
-  <h1>Boilerplate app!</h1>,
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+      
+    </Route>
+  </Router>,
+
   document.getElementById('app')
 );
---
+++ app/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = React.createClass ({
+    render () {
+        return (
+            <h2>Main</h2>
+        );
+    }
+});
+
+module.exports = Main;
