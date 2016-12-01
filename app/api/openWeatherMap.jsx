d7794909c40274bd53546f8323802a9b560503db
priom <priom.galaxy@gmail.com>
2016-12-01 16:01:15 -0400

    openweather api fetched

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,3 +1,20 @@
-// import { axios } from 'axios';
+import axios from 'axios';
 
-// const
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=a0cac478c7c1574074da78edf76a5ce1&units=metric';
+
+module.exports = {
+    getTemp: function (location) {
+        const encodedLocation = encodeURIComponent(location);
+        const requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+        return axios.get(requestUrl).then(function (res) {
+            if (res.data.cod && res.data.message) {
+                throw new Error(res.data.message);
+            } else {
+                return res.data.main.temp;
+            }
+        }, function (res) {
+            throw new Error(res.data.message);
+        });
+    }
+}
