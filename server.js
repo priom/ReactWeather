cdbe32697e255abbf627f2a9e34ef03348b01a14
priom <priom.galaxy@gmail.com>
2016-12-02 17:19:53 -0400

    server configured

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'http') {
+    if (req.headers['x-forwarded-proto'] === 'https') {
         res.redirect('http://' + req.hostname + req.url);
     } else {
         next();
