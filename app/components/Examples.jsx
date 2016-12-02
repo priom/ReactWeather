a524f38baaed65ae42adfbaedd0c700ac6923c45
priom <priom.galaxy@gmail.com>
2016-12-02 18:14:28 -0400

    nav styled

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Examples = () => {
     return (
-        <h3>Examples</h3>
+        <div>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try.</p>
+            <ol>
+                <li>
+                    <Link to="/?location=Toronto"> Toronto, ON</Link>
+                </li>
+                <li>
+                    <Link to="/?location=NewYork"> New York, NY</Link>
+                </li>
+            </ol>
+        </div>
     );
 };
 
